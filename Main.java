d7e771f2bccfbe932dd2c7eaf86b6b87e24db5c7
Chad Niedert <>
2021-04-09 02:24:00 +0000

    completed problem

--- Main.java
+++ Main.java
@@ -1,7 +1,19 @@
 // Link to problem: https://projecteuler.net/problem=5
 
 class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
-  }
+  	public static void main(String[] args) {
+    	boolean flag = false;
+		int num = 1;
+
+		while (flag == false)
+		{
+			if (num % 2 == 0 && num % 3 == 0 && num % 4 == 0 && num % 5 == 0 && num % 6 == 0 && num % 7 == 0 && num % 8 == 0 && num % 9 == 0 && num % 10 == 0 && num % 11 == 0 && num % 12 == 0 && num % 13 == 0 && num % 14 == 0 && num % 15 == 0 && num % 16 == 0 && num % 17 == 0 && num % 18 == 0 && num % 19 == 0 && num % 20 == 0)
+			{
+				flag = true;
+				System.out.println(num);
+			}
+			num++;
+		}
+
+  	}
 }
